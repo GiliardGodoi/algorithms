469774f69666ce8ba80ce316079757d2a62938cd
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-11-30 15:01:31 -0200

    alteração em test_angle_with. lembrar de testar as exceções geradas e com valores quebrados para validadar questões de arredondamento e etc.

--- python/vector/test.py
+++ python/vector/test.py
@@ -64,6 +64,10 @@ class VectorTest(ut.TestCase):
         self.assertEqual(v.angle_with(w),(math.pi/2))
         self.assertEqual(v.angle_with(w,in_degrees=True),90)
 
+        t = Vector([0,0,0])
+        with self.assertRaises(Exception) as cm:
+            v.angle_rad(t)
+
     def test_is_zero(self):
         v1 = Vector([0,0,0])
         self.assertTrue(v1.is_zero())
@@ -110,46 +114,40 @@ class VectorTest(ut.TestCase):
         self.assertTrue(v4.is_parallel_to(w4))
         self.assertTrue(v4.is_orthogonal_to(w4))
 
-def test_magnitude_vetor_unitario():
-    v1 = Vector([-0.221, 7.437])
-    tamV1 = v1.magnitude()
-    print(tamV1)
+    def test_magnitude_vetor_unitario(self):
+        v1 = Vector([-0.221, 7.437])
+        tamV1 = v1.magnitude()
+        print(tamV1)
 
-    v2 = Vector([8.813, -1.331, -6.247])
-    print(v2.magnitude())
+        v2 = Vector([8.813, -1.331, -6.247])
+        print(v2.magnitude())
 
-    v3 = Vector([5.581, -2.136])
-    print(v3.normalized())
+        v3 = Vector([5.581, -2.136])
+        print(v3.normalized())
 
-    v4 = Vector([1.996, 3.108, -4.554])
-    print(v4.normalized())
+        v4 = Vector([1.996, 3.108, -4.554])
+        print(v4.normalized())
 
-def dot_product_and_angle():
-    print('DOT PRODUCT \n\tExercicio 1')
-    v1 = Vector([7.887, 4.138])
-    w1 = Vector([-8.802, 6.776])
-    print(v1.inner_product(w1))
+    def dot_product_and_angle(self):
+        print('DOT PRODUCT \n\tExercicio 1')
+        v1 = Vector([7.887, 4.138])
+        w1 = Vector([-8.802, 6.776])
+        print(v1.inner_product(w1))
 
-    print('\tExercicio 2')
-    v2 = Vector([-5.955, -4.904, -1.874])
-    w2 = Vector([-4.496, -8.755, 7.103])
-    print(v2.inner_product(w2))
+        print('\tExercicio 2')
+        v2 = Vector([-5.955, -4.904, -1.874])
+        w2 = Vector([-4.496, -8.755, 7.103])
+        print(v2.inner_product(w2))
 
-    print("\n\nANGLE\n\tExercicio 3")
-    v3 = Vector([3.183, -7.627])
-    w3 = Vector([-2.669, 5.319])
-    print(v3.angle_rad(w3))
+        print("\n\nANGLE\n\tExercicio 3")
+        v3 = Vector([3.183, -7.627])
+        w3 = Vector([-2.669, 5.319])
+        print(v3.angle_rad(w3))
 
-    print('\tExercicio 4')
-    v4 = Vector([7.35, 0.221, 5.188])
-    w4 = Vector([2.751, 8.259, 3.985])
-    print(v4.angle_degrees(w4))
-
-
-# #test_magnitude_vetor_unitario()
-# #dot_product_and_angle()
-# #dot_product_vector_zero()
-
+        print('\tExercicio 4')
+        v4 = Vector([7.35, 0.221, 5.188])
+        w4 = Vector([2.751, 8.259, 3.985])
+        print(v4.angle_degrees(w4))
 
 if __name__ == "__main__" :
     ut.main()
