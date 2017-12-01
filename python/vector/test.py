ee274e86396d88c296a2557c2731e40a339c0fda
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2017-12-01 08:20:11 -0200

    alteracao de test_parallel_or_orthogonal. adicionando testes para verificar se o angulo entre dois vetores bate com a definição de vetores ortogonais e paralelos

--- python/vector/test.py
+++ python/vector/test.py
@@ -98,56 +98,50 @@ class VectorTest(ut.TestCase):
         w1 = Vector([22.737, 23.64])
         self.assertTrue(v1.is_parallel_to(w1))
         self.assertFalse(v1.is_orthogonal_to(w1))
+        self.assertEqual(v1.angle_with(w1,in_degrees=True),180)
 
         v2 = Vector([-2.029, 9.97, 4.172])
         w2 = Vector([-9.231, -6.639, -7.724])
         self.assertFalse(v2.is_parallel_to(w2))
         self.assertFalse(v2.is_orthogonal_to(w2))
+        self.assertNotEqual(v2.angle_with(w2, in_degrees=True),0)
+        self.assertNotEqual(v2.angle_with(w2, in_degrees=True),90)
 
         v3 = Vector([-2.328, -7.284, -1.214])
         w3 = Vector([-1.821, 1.072, -2.94])
         self.assertFalse(v3.is_parallel_to(w3))
         self.assertTrue(v3.is_orthogonal_to(w3))
+        self.assertEqual(v3.angle_with(w3, in_degrees=True),90)
 
         v4 = Vector([2.118, 4.827])
         w4 = Vector([0,0])
         self.assertTrue(v4.is_parallel_to(w4))
         self.assertTrue(v4.is_orthogonal_to(w4))
+        with self.assertRaises(Exception) as expt :
+            v4.angle_with(w4,in_degrees=True)
 
     def test_magnitude_vetor_unitario(self):
         v1 = Vector([-0.221, 7.437])
         tamV1 = v1.magnitude()
-        print(tamV1)
 
         v2 = Vector([8.813, -1.331, -6.247])
-        print(v2.magnitude())
 
         v3 = Vector([5.581, -2.136])
-        print(v3.normalized())
 
         v4 = Vector([1.996, 3.108, -4.554])
-        print(v4.normalized())
 
     def dot_product_and_angle(self):
-        print('DOT PRODUCT \n\tExercicio 1')
         v1 = Vector([7.887, 4.138])
         w1 = Vector([-8.802, 6.776])
-        print(v1.inner_product(w1))
 
-        print('\tExercicio 2')
         v2 = Vector([-5.955, -4.904, -1.874])
         w2 = Vector([-4.496, -8.755, 7.103])
-        print(v2.inner_product(w2))
 
-        print("\n\nANGLE\n\tExercicio 3")
         v3 = Vector([3.183, -7.627])
         w3 = Vector([-2.669, 5.319])
-        print(v3.angle_rad(w3))
 
-        print('\tExercicio 4')
         v4 = Vector([7.35, 0.221, 5.188])
         w4 = Vector([2.751, 8.259, 3.985])
-        print(v4.angle_degrees(w4))
 
 if __name__ == "__main__" :
     ut.main()
