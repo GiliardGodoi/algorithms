a74c812f3ff008c385a55312b444d2110d18c3b3
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-04-11 08:44:54 -0300

    Adicionando propriedade __len__ a particle

--- python/GA/projetofinal/particle.py
+++ python/GA/projetofinal/particle.py
@@ -3,15 +3,20 @@ import numpy as np
 class Particle(object):
 
     def __init__(self,position,velocity,**kwargs):
+        assert len(position) == len(velocity), "Position and velocity must have the same dimension"
         self.__position = np.array(position)
         self.__velocity = np.array(velocity)
         self.pbest_position = np.array(self.position)
+        self.__dimension = len(self.__velocity)
 
     def __str__(self):
         return f'{self.__position}'
 
     def __repr__(self):
         return self.__str__()
+
+    def __len__(self):
+        return len(self.__dimension)
 
     @property
     def position(self):
