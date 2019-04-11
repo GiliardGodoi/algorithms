5b000828844b97131b5a80c6c4b488107856559e
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-04-11 11:53:39 -0300

    renomeando funções

--- python/GA/projetofinal/util/benchmarks.py
+++ python/GA/projetofinal/util/benchmarks.py
@@ -3,14 +3,14 @@ import numpy as np
 def dejong_sphere(variables):
         return np.sum(np.power(variables,2))
 
-def quadratic_random(variables):
+def quadratic_noise(variables):
     random_vector = np.random.uniform(0,1,size=len(variables))    
     return np.sum(np.power(variables,4) + random_vector)
 
-def quadratic_cosine(variables):
+def rastrigin_function(variables):
     return np.sum((np.power(variables,2)) - (10 * np.cos(2*np.pi*variables) + 10))
 
-def other_more_complicate(variables):
+def griewank_function(variables):
     const = 1/4000
     indices = np.arange(1,len(variables)+1,1,dtype=np.int16)
 
