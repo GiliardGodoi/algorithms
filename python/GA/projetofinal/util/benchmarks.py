6ae952b14a44e52d04fe5eacd062485b81b6d02b
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-04-16 23:58:42 -0300

    refact: quadratic_noise

--- python/GA/projetofinal/util/benchmarks.py
+++ python/GA/projetofinal/util/benchmarks.py
@@ -4,8 +4,9 @@ def dejong_sphere(variables):
         return np.sum(np.power(variables,2))
 
 def quadratic_noise(variables):
-    random_vector = np.random.uniform(0,1,size=len(variables))    
-    return np.sum(np.power(variables,4) + random_vector)
+    random_vector = np.random.uniform(0,1,size=len(variables))
+    integer_vector = np.arange(1,len(variables) + 1)
+    return np.sum(integer_vector * np.power(variables,4) + random_vector)
 
 def rastrigin_function(variables):
     N = len(variables)
@@ -16,7 +17,7 @@ def griewank_function(variables):
     indices = np.arange(1,len(variables)+1,1,dtype=np.int16)
 
 #     somatorio = np.sum(indices * np.power(variables,2))
-    somatorio = np.sum(const * np.power(variables,2))
+    somatorio = const * np.sum(np.power(variables,2))
     produtorio = np.prod(np.cos(np.divide(variables,np.sqrt(indices))))
 
     return 1 + somatorio - produtorio
