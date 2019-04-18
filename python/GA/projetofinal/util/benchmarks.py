0c6e10673f27c3bdf4e0bc486557199fb189fe90
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-04-17 22:27:50 -0300

    feat: noncontinuous rastrigin

--- python/GA/projetofinal/util/benchmarks.py
+++ python/GA/projetofinal/util/benchmarks.py
@@ -8,15 +8,29 @@ def quadratic_noise(variables):
     integer_vector = np.arange(1,len(variables) + 1)
     return np.sum(integer_vector * np.power(variables,4) + random_vector)
 
+def rastrigin(variables):
+    return np.sum((np.power(variables,2)) + (-10 * np.cos(2 * np.pi * variables)) + 10)
+
 def rastrigin_function(variables):
     N = len(variables)
     return (10 * N) + np.sum((np.power(variables,2)) - (10 * np.cos(2 * np.pi * variables)))
 
+def _rastring_transf_function(x):
+    if np.abs(x) >= 0.5 :
+        return np.divide(np.round(2 * x),2)
+    else :
+        return x
+
+noncontinuous_transformation = np.vectorize(_rastring_transf_function)
+
+def rastrigin_noncontinuous(variables):
+    Y = noncontinuous_transformation(variables)
+    return rastrigin(Y)
+
 def griewank_function(variables):
-    const = 1/4000
+    const = np.divide(1,4000)
     indices = np.arange(1,len(variables)+1,1,dtype=np.int16)
 
-#     somatorio = np.sum(indices * np.power(variables,2))
     somatorio = const * np.sum(np.power(variables,2))
     produtorio = np.prod(np.cos(np.divide(variables,np.sqrt(indices))))
 
