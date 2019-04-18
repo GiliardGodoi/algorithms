405a815925d790b79be94b3a43d813989105d860
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-04-18 15:04:00 -0300

    simulação boa Constriction

--- python/GA/projetofinal/simulacao.py
+++ python/GA/projetofinal/simulacao.py
@@ -1,5 +1,5 @@
 from pso import SearchSpace
-from util.benchmarks import dejong_sphere as fitness
+from util.benchmarks import griewank_function as fitness
 from util.visualization import line_plot
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,14 +8,16 @@ import time
 if __name__ == "__main__":
     
     pso = SearchSpace(costFunction=fitness,
-                        nroParticles=50,
-                        maxIteration=10000,
+                        nroParticles=100,
+                        maxIteration=2000,
                         dimensions=20,
-                        bounds=[-5.12,5.12],                
+                        bounds=[-5.12,5.12],
+                        velocityStrategy="CONSTRICTION"
                     )
 
-    pso.set_updateStrategiesParams(c1=0.05,c2=0.5,kappa=1,w=0.5)
-    pso.setup() # Inicilaiza as particulas e as estratégias
+    # pso.set_updateStrategiesParams(c1=1.5,c2=1.5,kappa=1,w=0.5,w_min=0.4,w_max=0.9)
+    pso.set_updateStrategiesParams(c1=2.05,c2=2.05,c3=0.5,kappa=1,w=0.5,w_min=0.4,w_max=0.9)
+    pso.setup() # Inicilaiza as estratégias
 
     MAX_ITERATION =  5
     iteration = 0
