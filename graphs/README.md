32920f9de3ca083e76454e33e56e3ff81f96be86
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2019-06-07 22:57:22 -0300

    INTRODUZINDO ESTUDOS SOBRE GRAFOS
     Your branch is up to date with 'origin/graphs'.
    
     Mudanças a serem submetidas:
    	new file:   graphs/README.md
    	new file:   graphs/data/movies.txt
    	new file:   graphs/graph.py

---
+++ graphs/README.md
@@ -0,0 +1,8 @@
+# Grafos
+
+## Pequeno estudos sobre grafos
+
+
+## Referências
+
+- A Case Study: Small Word Phenomenon. Acessado em: <https://introcs.cs.princeton.edu/python/45graph/>
---
+++ graphs/graph.py
@@ -0,0 +1,59 @@
+import sys
+import os
+
+class Graph:
+
+    def __init__(self,filename=None,delimiter=None):
+        self._edges = 0
+        self._adj = dict()
+
+        if filename is not None :
+            pass
+
+
+    def add_edge(self,v,w):
+        if not self.has_vertex(v) : self._adj[v] = set()
+        if not self.has_vertex(w) : self._adj[w] = set()
+        if not self.has_edge(v,w):
+            self._edges += 1
+            self._adj[v].add(w)
+            self._adj[w].add(v)
+
+            return True
+
+        return False
+
+    def adjacent_to(self,v):
+        return iter(self._adj[v])
+
+    def vertices(self):
+        return iter(self._adj)
+
+    def has_vertex(self,v):
+        return (v in self._adj)
+
+    def has_edge(self,v,w):
+        return (w in self._adj[v])
+
+    def count_edges(self):
+        return self._edges
+
+    def __len__(self):
+        return self.count_edges()
+
+    def degree(self,v):
+        return len(self._adj[v])
+
+    def __str__(self):
+        s = ''
+
+        for v in self.vertices():
+            s += (str(v) + ' : ')
+            for w in self.adjacent_to(v):
+                s += (str(w) + ' ')
+            s += '\n'
+
+        return s
+
+    def __repr__(self):
+        return self.__str__()
